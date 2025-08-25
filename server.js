c7dd4e6a5284b0d60879dd274e5f4870c0becb7c
Uchiha-Itachi001 <pankojrgit06@gmail.com>
2025-08-26 00:30:48 +0530

    modify

--- server.js
+++ server.js
@@ -1,38 +1,41 @@
 require("dotenv").config();
+//!modules
+
 const express = require("express");
 const connectDB = require("./config/db");
 const cors = require("cors");
+const router = require("./routers/uplode");
 const path = require("path");
 const fs = require("fs");
-
-const uploadRouter = require("./routers/uplode");
 const Studentrouter = require("./routers/StudentRouterds");
 const authRouter = require("./routers/authRouts");
-
+const authMiddleware = require("./middleware/authMiddleware");
+const { UserData } = require("./controllers/authControllers");
 const app = express();
+ 
+app.use("/uploads", express.static("uploads"));
 
-// Middlewares
+//@middlewares
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Static uploads folder
-app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+const uploaddir = path.join(__dirname, "uploads");
+if (!fs.existsSync(uploaddir)) {
+  fs.mkdirSync(uploaddir);
+}
 
-// Routers
-app.use("/api/auth", uploadRouter);
+//@Routers
+app.use("/api/auth", router);
 app.use("/api/authentication", authRouter);
 app.use("/api/students", Studentrouter);
 
-// Test route
-app.get("/", (req, res) => {
-  res.send("Hello 👋 Server running...");
-});
-
-// DB + Start
+app.get("/",(req,res)=>{
+    res.send("HEllo")
+})
 connectDB().then(() => {
-  const port = process.env.PORT || 5000;
+  const port = 5000;
   app.listen(port, () => {
-    console.log(`✅ Server is running on port ${port}`);
+    console.log(`Server is running on port ${port}`);
   });
 });
