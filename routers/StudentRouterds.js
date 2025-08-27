2f4cd5213d38bc3e9b25f0dc24a8f51961727ffb
Uchiha-Itachi001 <pankojrgit06@gmail.com>
2025-08-27 13:47:30 +0530

    change the route delete to post

--- routers/StudentRouterds.js
+++ routers/StudentRouterds.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const Studentrouter = express.Router();
 const multer = require("multer");
-const { add, List, deleteAll, deleteitem, addId, UserID } = require("../controllers/StudentControllers");
-const { deleteMany } = require("../models/uplode_models");
+const { add, List, deleteAll, deleteitem, addId, UserID,deleteMany } = require("../controllers/StudentControllers");
+
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {  // <-- Use 'cb' instead of 'cd'
@@ -20,7 +20,7 @@ Studentrouter.post("/upload", uploads.single("file"), add);
 Studentrouter.get("/data", List);
 Studentrouter.post("/delete", deleteitem); // New delete route
 Studentrouter.delete("/delete-all", deleteAll); // New delete route
-Studentrouter.delete("/deleteMany", deleteMany);
+Studentrouter.post("/deleteMany", deleteMany);
 Studentrouter.post("/notify", addId); // New delete route
 Studentrouter.post("/user", UserID); // New delete route
 module.exports = Studentrouter;
